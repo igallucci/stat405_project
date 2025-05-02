5c4364e8e6b52221bb2d6fce8aaa527e03368eef
JacksonCDataScience <145865766+JacksonCDataScience@users.noreply.github.com>
2025-05-01 22:36:59 -0500

    Update find_top_5.sh

--- find_top_5.sh
+++ find_top_5.sh
@@ -4,7 +4,7 @@
 counter=1
 
 while IFS= read -r file; do
-  full_file_path="/home/jwcramer2/project/$file" # If you want to prepend the current directory path
+  full_file_path="/home/jwcramer2/project/$file" # change according to your path
   tail -n +2 "$full_file_path" | cut -d ',' -f1 | sort | uniq -c > "out${counter}.txt"
   ((counter++))
 done < ./files.txt
